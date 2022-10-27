52359359290ab527f28a8647caf7333ab0902bf1
Max <1998max7362@mail.ru>
2022-10-27 23:44:52 +0300

    Start

---
+++ MainWindow/MainWindowFNCW.py
@@ -0,0 +1,12 @@
+import sys, os
+from PyQt5.QtWidgets import *
+from PyQt5 import QtCore
+from PyQt5.QtCore import Qt
+import numpy as np
+from PyQt5 import QtWidgets
+
+# # sys.path.insert(0, "././Core/")
+# sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'Core'))
+# from Clamp import Clamp
+
+# print('a')
