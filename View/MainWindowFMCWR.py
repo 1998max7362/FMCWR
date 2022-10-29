4e462a7bed23257dbfb53ab8d49ba7c9194d2b7d
1998max7362 <1998max7362@mail.ru>
2022-10-29 10:44:06 +0300

    dsf

--- View/MainWindowFMCWR.py
+++ View/MainWindowFMCWR.py
@@ -14,9 +14,15 @@ class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setWindowTitle('Главное меню')
+
+        self._createActions()
+        self._connectActions()
+        self._createMenubar()
+
         layout = QHBoxLayout(self)
         self.settings = SettingsWindow()
-        self.dockSettings = QDockWidget("Настройки")
+        # self.dockSettings = QDockWidget("Настройки")
+        self.dockSettings = QDockWidget()
         self.dockSettings.setWidget(self.settings)
         self.addDockWidget(Qt.LeftDockWidgetArea, self.dockSettings)
 
@@ -36,7 +42,25 @@ class MainWindow(QMainWindow):
     def SendPeriod(self,smth):
         print(smth)
 
-        
+    def _createMenubar(self):
+        menuBar = self.menuBar()
+        MainWindowMenuBar = menuBar.addMenu("&Файл")
+        MainWindowMenuBar.addAction(self.saveAction)
+        MainWindowMenuBar.addAction(self.loadAction)
+
+    def _createActions(self):
+        self.saveAction = QAction("&Сохранить",self)
+        self.loadAction = QAction("&Загрузить",self)
+    
+    def _connectActions(self):
+        self.saveAction.triggered.connect(self.saveFile)
+        self.loadAction.triggered.connect(self.loadFile)
+
+    def saveFile(self):
+        print('save')
+    
+    def loadFile(self):
+        print('load')
 
 
 if __name__ == '__main__':
