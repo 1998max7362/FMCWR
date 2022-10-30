b009d713c1c18fe63aaea1d0e098cd2a95a0d1ef
isaackatz <vlado2k16@gmail.com>
2022-10-31 00:26:41 +0300

    waterfall improved

--- View/MainWindowFMCWR.py
+++ View/MainWindowFMCWR.py
@@ -20,19 +20,23 @@ class MainWindow(QMainWindow):
         layout = QHBoxLayout(self)
         self.settings = SettingsWindow()
         self.dockSettings = QDockWidget("Настройки")
-        self.dockSettings.setWidget(self.settings)
-        self.addDockWidget(Qt.LeftDockWidgetArea, self.dockSettings)
-
         self.dockGraph0 = QDockWidget("График 0")
         self.dockGraph1 = QDockWidget("График 1")
         self.Chart0 = GraphWindow()
-        self.Chart0.setMinimumSize(300,200)
         self.Chart1 = WaterFallWindow()
+        self.Chart0.setMinimumSize(300,200)
         self.Chart1.setMinimumSize(300,200)
+        self.dockSettings.setWidget(self.settings)
         self.dockGraph0.setWidget(self.Chart0)
         self.dockGraph1.setWidget(self.Chart1)
+        self.addDockWidget(Qt.LeftDockWidgetArea, self.dockSettings)
         self.addDockWidget(Qt.RightDockWidgetArea, self.dockGraph0)
         self.addDockWidget(Qt.RightDockWidgetArea, self.dockGraph1)
+
+        # включение демоверсии
+        self.demo = Clamp()
+        self.demo.ConnectTo(self.Chart1.demo)
+        self.demo.Send(True)
 
         self.settings.Period.LineEdit.Text.HandleWithSend(self.SendPeriod)
 
--- View/mainWaterfall.py
+++ View/mainWaterfall.py
@@ -1,4 +1,5 @@
-from lib2to3.pgen2.token import NT_OFFSET
+import sys
+sys.path.insert(0, "././Core/")
 from pyqtspecgram import pyqtspecgram
 import pyqtgraph as pg
 import numpy as np
@@ -9,50 +10,74 @@ from PyQt5 import QtWidgets
 from PyQt5.QtWidgets import *
 import sys
 import matplotlib.pyplot as plt
+from Clamp import Clamp
 
 
 class WaterFallWindow(QWidget):
     def __init__(self):
         super().__init__()
+        # Проверка демоверсии
+        self.demo = Clamp()
+        self.demo.HandleWithReceive(self.receiveDemo)
         # Разметка окна
         wFallWindowLayout = QHBoxLayout(self)
         # plotWidget
         self.graphWidget = pg.PlotWidget()
-        # показать тестовый сигнал
-        s, fs  = self.createTestSignal()
-        # посчитать спектрограмму
+        self.graphWidget.setBackground('w')
+        # настройки спектрограммы
+        self.fs = 192e3
         tSeg = 0.1
-        nPerseg = int(tSeg*fs)
-        nfft = 100*nPerseg
-        f, t, Sxx = signal.spectrogram(s, fs, nperseg=nPerseg,nfft=nfft,scaling='spectrum')
-        img = pg.ImageItem()
-        img.setImage(Sxx)
-        cm = pg.colormap.get('CET-L9')
-        img.setColorMap(cm)
+        self.nPerseg = int(tSeg*self.fs)
+        self.nfft = 100*self.nPerseg
+        # рассчитать тестовый сигнал
+        s = self.createTestSignal()
+        # расчет спектрограммы
+        img = self.specImage(s)
         # Подключение виджета к разметке 
         wFallWindowLayout.addWidget(self.graphWidget)
         # Построение спектрограммы
         self.graphWidget.addItem(img)
 
-        # методы класса
+    # методы класса
     def createTestSignal(self):
         # Тестовый сигнал для водопада
         pi = np.pi
-        fs = 192e3
-        tvec = np.arange(0,5,1/fs)
+        fs = self.fs
+        tvec = np.arange(0,1,1/fs)
         m = 0.3
         f0 = 40e3
         f1 = 5e3
         Un = 100
-        testSig = Un*(1+m*np.cos(2*pi*f1*tvec))*np.cos(2*pi*f0*tvec)
-        hz = np.linspace(0, fs/2, int(np.floor(len(testSig)/2)+1))
+        testSig = Un*(1+m*np.cos(2*pi*f1*tvec,dtype='complex128'))*np.cos(2*pi*f0*tvec, dtype='complex128')
         pnts = len(testSig)
+        noise = 1*(np.random.randn(pnts) + 1j*np.random.randn(pnts))
+        testSig += noise
+        hz = np.linspace(0, fs/2, int(np.floor(len(testSig)/2)+1))
         spectra = 2*np.abs(sp.fft.fft(testSig)/pnts)
-        #plt.plot(tvec[:100], testSig[:100])
-        #plt.show()
-        #plt.plot(hz, spectra[:len(hz)])
-        #plt.show()
-        return testSig, fs
+        return testSig
+    # построение спектрограммы
+    def specImage(self, s):
+        f, t, spectra = signal.spectrogram(np.real(s), self.fs, noverlap=0.1*self.nPerseg,nperseg=self.nPerseg,nfft=self.nfft,scaling='density')
+        logSpectra = 10*np.log10(spectra)
+        cm = pg.colormap.get('viridis')
+        img = pg.ImageItem()
+        img.setImage(logSpectra)
+        img.setColorMap(cm)
+        tr = pg.QtGui.QTransform()
+        tr.scale(np.max(t)/len(t), self.fs/self.nfft)
+        # вставить шкалу уровней
+        minv, maxv = np.nanmin(np.nanmin(logSpectra[logSpectra != -np.inf])), np.nanmax(np.nanmax(logSpectra))
+        bar = pg.ColorBarItem(interactive=True, values=(minv, maxv), colorMap=cm, label='Мощность [дБ]')
+        bar.setImageItem(img, insert_in=self.graphWidget.plotItem)  
+        img.setTransform(tr)
+        self.graphWidget.plotItem.setLabel(axis='left', text='Частота, Гц')
+        self.graphWidget.plotItem.setLabel(axis='bottom', text='Время, с')
+        return img
+
+    def receiveDemo(self, data: bool):
+        self.demo.ReceivedValue = data
+
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
