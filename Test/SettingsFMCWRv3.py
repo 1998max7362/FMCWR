8c2113329b2473e57839b5b8fe833d6d76da162a
Max <1998max7362@mail.ru>
2023-09-24 14:35:24 +0300

    dfsdf

--- Test/SettingsFMCWRv3.py
+++ Test/SettingsFMCWRv3.py
@@ -1,5 +1,6 @@
 import sys
-sys.path.insert(0, "././Core/")
+sys.path.insert(0, "././utils/constants/")
+sys.path.insert(0, "././utils/components/")
 from PyQt5.QtWidgets import *
 from PyQt5 import QtCore
 from PyQt5.QtCore import Qt
